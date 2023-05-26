6e88be083cd761a26f970f9922987dc4d6f21e39
ccbaumler <ccbaumler@ucdavis.edu>
2023-05-26 00:01:28 -0700

    added more report info

--- clean-gtdb.snakefile
+++ clean-gtdb.snakefile
@@ -4,7 +4,11 @@
 #
 # could include a config file for easy changing of databases instead of the list below?
 
+
+
 DB_V=['gtdb-rs207.genomic-reps.dna.k31'] #add any number of gtdb sourmash databases to this list to process them all. but line 13?
+
+#configfile: "config.yml"
 
 rule all:
     input: expand("db/{gtdb}.clean.zip", gtdb=DB_V)
@@ -20,15 +24,18 @@ rule download_sourmash_gather_database:
     '''
 
 rule download_all_available_genbank_genomes:
-    output: "db/assembly_summary_genbank.txt"
+    output: 
+        full_db = "db/assembly_summary_genbank.txt",
+        supressed_db = "db/assembly_summary_genbank_historical.txt",
     threads: 1
     benchmark:
-        "benchmarks/download_all_available_genbank_genomes.txt"
+        "benchmarks/0_download_all_available_genbank_genomes.txt"
     resources:
         mem_mb = 1000,
         time_min = 30
     shell:'''
-        rsync -t -v rsync://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_genbank.txt {output}
+        rsync -t -v rsync://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_genbank.txt {output.full_db}
+        rsync -t -v rsync://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_genbank_historical.txt {output.supressed_db}
     '''
 
 rule sourmash_manifest_destiny:
@@ -36,7 +43,7 @@ rule sourmash_manifest_destiny:
     output: "manifest/{db}.mf.csv",
     conda: "envs/sourmash.yml"
     benchmark:
-        "benchmarks/manifest_{db}.txt"
+        "benchmarks/1_manifest_from_{db}.txt"
     resources:
         mem_mb = 8000,
         time_min = 30
@@ -46,30 +53,44 @@ rule sourmash_manifest_destiny:
 
 rule get_assembly_summary_identifiers:
     input:
-        db = "db/assembly_summary_genbank.txt",
+        db_good = "db/assembly_summary_genbank.txt",
+        db_bad = "db/assembly_summary_genbank_historical.txt",
     output: 
-        tmp = "manifest/assembly_summary.ident.txt",
+        good = "manifest/assembly_summary_genbank.good_ident.txt",
+        bad = "manifest/assembly_summary_genbank_historical.bad_ident.txt",
     benchmark:
-        "benchmarks/grepping_the_problem.txt"
+        "benchmarks/2_get_ass_identifiers.txt"
     resources:
         mem_mb = 8000,
         time_min = 30
     shell:'''
-        cut -f 1 {input.db} | awk 'NR>2' > {output.tmp}
+        cut -f 1 {input.db_good} | awk 'NR>2' > {output.good}
+        cut -f 1 {input.db_bad} | awk 'NR>2' > {output.bad}
     '''
 
 rule run_fun_script:
     input:
-        summary_idents = "manifest/assembly_summary.ident.txt",
+        summary_idents = "manifest/assembly_summary_genbank.good_ident.txt",
+        supressed_idents = "manifest/assembly_summary_genbank_historical.bad_ident.txt",
         mf = "manifest/{db}.mf.csv",
+        script = "scripts/munge-mf-with-idents.py",
     output:
         new_mf = "manifest/{db}.mf.clean.csv",
         report = "manifest/{db}.clean-report.txt"
     conda: "envs/sourmash.yml"
-    shell: """
-        ./scripts/munge-mf-with-idents.py {input.summary_idents} {input.mf} \
-            --report {output.report} -o {output.new_mf}
-    """
+    benchmark:
+        "benchmarks/3_run_fun_script_on_{db}.txt"
+    resources:
+        mem_mb = 8000,
+        time_min = 30
+    params: genbank_time="db/assembly_summary_genbank.txt"
+        #genbank_time="last modified: %s" % time.ctime(os.path.getmtime("db/assembly_summary_genbank.txt"))
+    script: "scripts/munge-mf-with-idents.py"
+
+#   shell: """
+#       {input.script} {input.summary_idents} {input.mf} \
+#           --report {output.report} -o {output.new_mf}
+#    """
 
 
 rule picklist_picnic:
@@ -80,7 +101,7 @@ rule picklist_picnic:
         woohoo = "db/{db}.clean.zip"
     conda: "envs/sourmash.yml"
     benchmark:
-         "benchmarks/picklist_picnic_{db}.txt"
+         "benchmarks/4_picklist_picnic_{db}.txt"
     resources:
         mem_mb = 8000,
         time_min = 30
--- scripts/munge-mf-with-idents.py
+++ scripts/munge-mf-with-idents.py
@@ -2,22 +2,28 @@
 import sys
 import argparse
 import csv
-
+import os.path
+import time
 import sourmash
 from sourmash import manifest # also does 'import sourmash'
 
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('good_idents', help='list of valid identifiers')
-    p.add_argument('old_mf', help='existing sourmash database manifest')
-    p.add_argument('--report', help='details of removed etc., for hoomans')
-    p.add_argument('-o', '--output',
-                   help='manifest cleansed of the impure')
+
+    # Add command-line arguments with default values from Snakemake
+    p.add_argument('good_idents', nargs='?', default=snakemake.input.summary_idents, help='list of valid identifiers')
+    p.add_argument('bad_idents', nargs='?', default=snakemake.input.supressed_idents, help='list of suppressed identifiers')
+    p.add_argument('old_mf', nargs='?', default=snakemake.input.mf, help='existing sourmash database manifest')
+    p.add_argument('--report', nargs='?', default=snakemake.output.report, help='details of removed etc., for humans')
+    p.add_argument('-o', '--output', nargs='?', default=snakemake.output.new_mf, help='manifest cleansed of the impure')
     args = p.parse_args()
 
     good_idents = set()
     good_idents_no_version = set()
+
+    bad_idents = set()
+    bad_idents_no_version = set()
 
     def get_suffix(name):
         ident = name.split(' ')[0]
@@ -41,17 +47,28 @@ def main():
             suffix_no_version = get_suffix_no_version(line)
             good_idents_no_version.add(suffix_no_version)
 
-    print(f"\n\nLoaded {len(good_idents)} genbank identifiers from '{args.good_idents}'",
-          file=sys.stderr)
-    print(f"(and loaded {len(good_idents_no_version)} identifiers without version number)", file=sys.stderr)
+    with open(args.bad_idents, 'rt') as fp:
+        for line in fp:
+            assert line.startswith('GC')
+            line = line.strip()
+            suffix = get_suffix(line)
+            bad_idents.add(suffix)
+
+            suffix_no_version = get_suffix_no_version(line)
+            bad_idents_no_version.add(suffix_no_version)
+
     assert len(good_idents) == len(good_idents_no_version)
 
+    #assert len(bad_idents) == len(bad_idents_no_version) This breaks the script! Why?
+    print(len(bad_idents_no_version))
+    print(len(bad_idents))
     old_mf = manifest.BaseCollectionManifest.load_from_filename(args.old_mf)
 
     ## now go through and filter
 
     removed_list = []
     updated_version_list = []
+    bad_list = []
 
     keep_rows = []
     n_changed_version = 0
@@ -68,14 +85,32 @@ def main():
                 updated_version_list.append(name)
             else:
                 removed_list.append(name)
+        
+        if suffix in bad_idents:
+            bad_list.append(row)
+
 
     n_removed = len(old_mf) - len(keep_rows)
     n_suspect_suspension = n_removed - n_changed_version
     new_mf = manifest.CollectionManifest(keep_rows)
     
+    #genbank_time = snakemake@params[['genbank_time']]
+    genbank_time = time.ctime(os.path.getmtime(snakemake.params.genbank_time)) 
+
+
+    print(f"\n\nFrom current GenBank genome assemblies:", file=sys.stderr)
+    print(f"Loaded {len(good_idents)} genbank identifiers from '{args.good_idents}'",
+                          file=sys.stderr)
+    print(f"(and loaded {len(good_idents_no_version)} identifiers without version number)", file=sys.stderr)
+    print(f"File last modified {genbank_time}", file=sys.stderr)
+
     print(f"\nFrom '{args.old_mf}':", file=sys.stderr)
     print(f"Kept {len(keep_rows)} of {len(old_mf)} identifiers.",
             file=sys.stderr)
+    
+    print(f"\nFrom '{args.bad_idents}':", file=sys.stderr)
+    print(f"Kept {len(bad_list)} of {len(bad_idents)} identifiers.", file=sys.stderr)
+    
     print(f"\nNew manifest '{args.output}':", file=sys.stderr)
     print(f"Kept {len(new_mf)} identifiers.", file=sys.stderr)
     print(f"Removed {n_removed} total identifiers.",
@@ -84,6 +119,7 @@ def main():
             file=sys.stderr)
     print(f"Removed {n_suspect_suspension} identifiers because of suspected suspension of the genome.\n\n",
             file=sys.stderr)
+
 
     with open(args.output, 'w', newline='') as outfp:
         new_mf.write_to_csv(outfp, write_header=True)
