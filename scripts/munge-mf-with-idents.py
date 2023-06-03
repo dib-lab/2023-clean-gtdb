8a71d5537a5d2ea39e19ffaec4355fa458896bf6
ccbaumler <ccbaumler@ucdavis.edu>
2023-06-02 22:54:16 -0700

    script includes dict and report needs fixing

--- scripts/munge-mf-with-idents.py
+++ scripts/munge-mf-with-idents.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+
 import sys
 import argparse
 import csv
@@ -13,7 +14,7 @@ def main():
 
     # Add command-line arguments with default values from Snakemake
     p.add_argument('good_idents', nargs='?', default=snakemake.input.summary_idents, help='list of valid identifiers')
-    p.add_argument('bad_idents', nargs='?', default=snakemake.input.supressed_idents, help='list of suppressed identifiers')
+    p.add_argument('bad_idents', nargs='?', default=snakemake.input.supressed_idents, help='list of suppressed and re-versioned identifiers')
     p.add_argument('old_mf', nargs='?', default=snakemake.input.mf, help='existing sourmash database manifest')
     p.add_argument('--report', nargs='?', default=snakemake.output.report, help='details of removed etc., for humans')
     p.add_argument('-o', '--output', nargs='?', default=snakemake.output.new_mf, help='manifest cleansed of the impure')
@@ -23,11 +24,13 @@ def main():
     good_idents_no_version = set()
 
     bad_idents = set()
-    bad_idents_no_version = set()
+    #bad_idents_no_version = set()
+
+    bad_idents_dict = {}
 
     def get_suffix(name):
         ident = name.split(' ')[0]
-        assert ident.startswith('GC')
+        assert ident.startswith('GC')  
         suffix = ident[3:]
         return suffix
 
@@ -49,29 +52,69 @@ def main():
 
     with open(args.bad_idents, 'rt') as fp:
         for line in fp:
-            assert line.startswith('GC')
             line = line.strip()
+            assert line.startswith('GC')
             suffix = get_suffix(line)
             bad_idents.add(suffix)
 
-            suffix_no_version = get_suffix_no_version(line)
-            bad_idents_no_version.add(suffix_no_version)
+            # Extract the version number from the suffix
+            assembly, version = suffix.split('.')
+
+            # Add the suffix and version as key-value pair to the dictionary
+            if assembly not in bad_idents_dict:
+                bad_idents_dict[assembly] = []
+            
+            bad_idents_dict[assembly].append(version)
 
     assert len(good_idents) == len(good_idents_no_version)
 
-    #assert len(bad_idents) == len(bad_idents_no_version) This breaks the script! Why?
-    print(len(bad_idents_no_version))
-    print(len(bad_idents))
+    assert len(bad_idents) == sum([len(k) for k in bad_idents_dict.values()])
+    
+    #print(len(bad_idents_dict.keys()))
+    #print(len(bad_idents))
+    
+    #print(sum([len(x) for x in bad_idents_dict.values()]))
+        
+    old_version_dict = {k: v for k, v in bad_idents_dict.items() if len(v) > 1}
+    #print(old_version_dict)
+
+#
+#    with open("bad_idents.txt", "w") as of:
+#            of.write("\n".join(bad_idents))
+#    with open("bad_idents_no_version.txt", "w") as of:
+#                    of.write("\n".join(bad_idents_no_version))
+#    with open("bad_idents_dict_keys.txt", "w") as of:
+#        of.write("\n".join(bad_idents_dict.keys()))
+#    with open("bad_idents_dict.csv", "w") as of:
+#        for k in sorted( bad_idents_dict ):
+#            for v in bad_idents_dict[k]:
+#                of.write(k + '.' + v + '\n' )
+#
     old_mf = manifest.BaseCollectionManifest.load_from_filename(args.old_mf)
 
     ## now go through and filter
 
+    bad_list = []
+    for k in bad_idents_dict:
+        for v in bad_idents_dict[k]:
+            bad_list.append( k + '.' + v )
+    #print(bad_list)
+
+    versioned_list = []
+    for k in old_version_dict:
+        for v in old_version_dict[k]:
+            versioned_list.append( k + '.' + v )
+    #print(versioned_list)
+
     removed_list = []
     updated_version_list = []
-    bad_list = []
-
     keep_rows = []
     n_changed_version = 0
+    
+    bad = []
+    suppressed_versioned = []
+    
+
     for row in old_mf.rows:
         name = row['name']
         suffix = get_suffix(name)
@@ -85,24 +128,43 @@ def main():
                 updated_version_list.append(name)
             else:
                 removed_list.append(name)
+        if suffix in bad_list:
+            bad.append(suffix)
+    #print(bad)
+
+    with open(snakemake.params.genbank_ver_status, "r", newline='') as fp:
+        # skip header
+        for x in range(2):
+            next(fp)
+        # create list from historical db    
+        doc = [line.strip().split('\t') for line in fp]
+
+    #print(doc)
+    for row in doc:
+        name = row[0]
+        suffix = get_suffix(name)
+
+        if suffix in bad:
+            suppressed_versioned.append(row)
         
-        if suffix in bad_idents:
-            bad_list.append(row)
 
-
+    print(len(suppressed_versioned))
+    
     n_removed = len(old_mf) - len(keep_rows)
     n_suspect_suspension = n_removed - n_changed_version
     new_mf = manifest.CollectionManifest(keep_rows)
     
     #genbank_time = snakemake@params[['genbank_time']]
-    genbank_time = time.ctime(os.path.getmtime(snakemake.params.genbank_time)) 
+    creat_time = time.ctime(os.path.getctime(snakemake.params.genbank_time))
+    mod_time = time.ctime(os.path.getmtime(snakemake.params.genbank_time)) 
 
 
-    print(f"\n\nFrom current GenBank genome assemblies:", file=sys.stderr)
-    print(f"Loaded {len(good_idents)} genbank identifiers from '{args.good_idents}'",
+    print(f"\n\nFrom genome assemblies database:", file=sys.stderr)
+    print(f"Loaded {len(good_idents)} identifiers from '{args.good_idents}'",
                           file=sys.stderr)
     print(f"(and loaded {len(good_idents_no_version)} identifiers without version number)", file=sys.stderr)
-    print(f"File last modified {genbank_time}", file=sys.stderr)
+    print(f"File assembly database created on {creat_time}", file=sys.stderr)
+    print(f"File assembly database last modified {mod_time}", file=sys.stderr)
 
     print(f"\nFrom '{args.old_mf}':", file=sys.stderr)
     print(f"Kept {len(keep_rows)} of {len(old_mf)} identifiers.",
@@ -133,6 +195,9 @@ def main():
                   file=fp)
             print(f"Removed {n_changed_version} because of changed version.",
                   file=fp)
+            print(f"---- {len(suppressed_versioned)} included into the bad list category.", file=fp)
+            for item in suppressed_versioned:
+                print("\n".join(str(i) for i in item), file=fp)
             print(f"---- {n_suspect_suspension} removed because presumed guilt ----", file=fp)
             print("\n".join(removed_list), file=fp)
             print(f"---- {n_removed} removed because version changed ----", file=fp)
